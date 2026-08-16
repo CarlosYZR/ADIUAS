b0ddd446bf32bf81e7093cac15c0627519d7ddb2
BanHammer-IV <banhammer.developer@hotmail.com>
2026-08-15 19:45:06 -0700

    Cambio rapido al Login y al CSS

--- Views/login.php
+++ Views/login.php
@@ -17,8 +17,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="../Assets/img/Logo_Uas.ico">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-    <link rel="stylesheet" href="../Assets/css/login.css">
+    <!--<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+--> <link rel="stylesheet" href="../Assets/css/login.css">
     <title>Iniciar sesion</title>
 </head>
 <body>
@@ -39,31 +39,18 @@
     </nav>
 
     
-    <div class="form-container">
-
+    <div class="form-login-container">
         <form action="../Controllers/ValidacionLogin.php" method="post">
-
-            <h1>Inicia sesion:</h1>
-
-            <div class="form-floating mb-3">
-
-                <input type="text" class="form-control" id="input_username" name="input_username" placeholder=" " required>
-
-
-                <label for="floatingInput" class="form-label">Usuario</label>
+            <div class="title">Inicia sesion</div>
+            <br>
+            <div>
+                <input type="text" class="form-control" id="input_username" name="input_username" placeholder="Usuario" required>
+                <input type="password" class="form-control" id="input_password" name="input_password" placeholder="Contraseña" required>
+                <a href="https://www.youtube.com/shorts/PUncwAjjPCc" target="_blank">Olvidaste tu contraseña?</a>
+                <input type="submit" value="Ingresar" class="btn">
             </div>
-
-            <div class="form-floating">
-                <input type="password" class="form-control" id="input_password" name="input_password" placeholder=" " required>
-                <label for="floatingPassword" class="form-label" >Contraseña</label>
-            </div>
-
-            <input type="submit" value="Ingresar" class="btn">
-
         </form>
-
     </div>
-
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
 </body>
 </html>
