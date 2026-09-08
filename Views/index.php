d313effea8816fe9747e15e23cbac6403935b940
Carlos <yazahar1853@gmail.com>
2026-09-07 18:25:58 -0600

    actualizado

--- Views/index.php
+++ Views/index.php
@@ -33,14 +33,12 @@
                   <span class="navbar-toggler-icon"></span>
                   </button>
                   <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                  <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
 
-                      <li class="nav-item">
-                          <a href="../index.html"><h2 class="item">Salir</h2></a>
-                      </li>
 
-                      <li class="nav-item">
-                          <a href="informes.php"><h2 class="item">Informes</h2></a>
+                        
+                      <li class="nav-item">   
+                        <a href="informes.php"> <h2 class="item">Informes</h2></a>
                       </li>
 
                       <ul class="navbar-nav">
@@ -121,6 +119,19 @@
               </button>
           </div>
       
+
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-12">
+                     <div class="card" style="width: 420px; height: 720px;">
+                            <div class="card-body">
+                                <h1 class="card-title">Eventos del Mes</h1>
+                                <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                            </div>
+                        </div>
+                     
+                  </div>
+              </div>
 
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
       </body>
--- Views/informes.php
+++ Views/informes.php
@@ -71,18 +71,6 @@
                           <a href="RegistroVisita.php"><h2 class="item">Registrar visita</h2></a>
                       </li>
       
-                      <!--
-                      <li class="nav-item dropdown">
-                          <a class="nav-link dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                              <h2>Unidades</h2>
-                          </a>
-                          <ul class="dropdown-menu">
-                              <li><a class="dropdown-item" href="#">Action</a></li>
-                              <li><a class="dropdown-item" href="#">Another action</a></li>
-                              <li><a class="dropdown-item" href="#">Something else here</a></li>
-                          </ul>
-                      </li> -->
-      
                     <ul class="navbar-nav">
                         <li class="nav-item dropdown">
                           
@@ -115,7 +103,7 @@
 
     <script>
         function eliminar(){
-            var respuesta = confirm("¿Esta seguro que desea eliminar el alumno?");
+            var respuesta = confirm("¿Estas seguro de eliminar este registro?");
             return respuesta;
         }
     </script>
